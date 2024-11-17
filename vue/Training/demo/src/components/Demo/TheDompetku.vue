8ef434178beaf5c2460cbd54c5ddaf1c5bc08658
agusrio <agusriochannel@gmail.com>
2024-11-17 20:42:36 +0700

    hasil training vue.js

---
+++ vue/Training/demo/src/components/Demo/TheDompetku.vue
@@ -0,0 +1,30 @@
+<script>
+import { ref, reactive } from 'vue'
+import MyTodoItem from './MyTodoItem.vue'
+
+const list = ref([
+  { id: 0, text: 'Sarapan pagi : ',  qty: 12.000, },
+  { id: 1, text: 'Makan Siang: ',  qty: 15.000 }, 
+  { id: 2, text: 'Ongkos Perjalanan : ',  qty: 30000 },
+])
+
+// const listText = ref('')
+const counterId = ref(3)
+const todoText = ref('')
+
+const submitFn = () => {
+  list.value.push({ id: counterId, text: todoText.value })
+  counterId.value++
+} 
+
+</script>
+
+<template>
+  <div>
+        <Input v-model="todoText" />
+        <button @click="submitFn">Add</button>
+        <ol>
+            <MyTodoItem v-for="item in list" :todo="item" :key="item.id"></MyTodoItem>
+        </ol>
+    </div>
+ </template>
---
+++ vue/Training/demo/src/views/DompetView.vue
@@ -0,0 +1,25 @@
+<!-- <template>
+  <div class="about">
+    <h1>This is an Dompetku page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style> -->
+
+<script setup>
+import TheDompetku from '../components/Demo/TheDompetku.vue'
+</script>
+
+<template>
+  <main>
+    <TheDompetku />
+  </main>
+</template>
